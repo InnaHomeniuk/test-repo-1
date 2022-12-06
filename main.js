a524c11deb9c0ac5569c0413388b4af0cce231a9
InnaHomeniuk <pivanova777@gmail.com>
2022-12-07 00:06:14 +0200

    added file main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+const number1 = Number(prompt('Уведіть перше число', ''));
+const number2 = Number(prompt('Уведіть друге число', ''));
+const number3 = Number(prompt('Уведіть третє число', ''));
+const resultAverage = (number1 + number2 + number3) / 3;
+alert(`Середнє арифметичне між числами: ${number1}, ${number2} i ${number3} складає:
+${resultAverage}`);
+console.log(`Середнє арифметичне між числами: ${number1}, ${number2} i ${number3} складає:
+${resultAverage}`);
